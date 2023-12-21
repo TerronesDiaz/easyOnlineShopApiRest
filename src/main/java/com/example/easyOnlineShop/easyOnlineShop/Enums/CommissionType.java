48eb350823301138481b56fb12ef253de718975e
TERRONES DIAZ FRANCISCO JAVIER <fterrones@ucol.mx>
2023-12-21 16:22:57 -0600

    Feature: New Enum to represent a fixed or percentage commission in a Digital Product.

---
+++ src/main/java/com/example/easyOnlineShop/easyOnlineShop/Enums/CommissionType.java
@@ -0,0 +1,6 @@
+package com.example.easyOnlineShop.easyOnlineShop.Enums;
+
+public enum CommissionType {
+    FIXED,       // Represents a fixed commission
+    PERCENTAGE   // Represents a percentage commission
+}
