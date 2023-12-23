f76f91f84a7b8b4713202b6a0ff656ac2774180e
TERRONES DIAZ FRANCISCO JAVIER <fterrones@ucol.mx>
2023-12-23 04:05:17 -0600

    Change: Refactored ProductController for improved structure and constructor-based dependency injection
    - Refactored the ProductController to improve code structure.
    - Introduced constructor-based dependency injection for the ProductService.
    - Added @CrossOrigin annotation for cross-origin resource sharing.

--- src/main/java/com/example/easyOnlineShop/easyOnlineShop/ProductController/ProductController.java
+++ src/main/java/com/example/easyOnlineShop/easyOnlineShop/ProductController/ProductController.java
@@ -3,6 +3,7 @@ package com.example.easyOnlineShop.easyOnlineShop.ProductController;
 import com.example.easyOnlineShop.easyOnlineShop.Dto.ProductDTO;
 import com.example.easyOnlineShop.easyOnlineShop.Service.ProductService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/v1/products")
 public class ProductController {
 
-    private ProductService productService;
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping(path = "/listProducts")
     public ResponseEntity<List<ProductDTO>> listProducts() {
