ad51f005d200756e70461131b29ca9178adf8cd8
TERRONES DIAZ FRANCISCO JAVIER <fterrones@ucol.mx>
2023-12-23 01:01:20 -0600

    Add: Created and implemented DigitalProductController to handle digital product operations
    - Added DigitalProductController to manage digital product-related endpoints.
    - Implemented listDigitalProducts to retrieve a list of digital products.
    - Implemented saveDigitalProduct to add a new digital product.
    - Implemented editDigitalProduct to update an existing digital product by ID.

---
+++ src/main/java/com/example/easyOnlineShop/easyOnlineShop/ProductController/DigitalProductController.java
@@ -0,0 +1,39 @@
+package com.example.easyOnlineShop.easyOnlineShop.ProductController;
+
+import com.example.easyOnlineShop.easyOnlineShop.Dto.DigitalProductDTO;
+import com.example.easyOnlineShop.easyOnlineShop.Service.DigitalProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/digitalProducts")
+public class DigitalProductController extends ProductController {
+
+    @Autowired
+    private DigitalProductService digitalProductService;
+
+    @GetMapping(path = "/listDigitalProducts")
+    public ResponseEntity<List<DigitalProductDTO>> listDigitalProducts() {
+        List<DigitalProductDTO> products = digitalProductService.findAllDigitalProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    @PostMapping(path = "/saveDigitalProduct")
+    public ResponseEntity<String> saveDigitalProduct(@RequestBody DigitalProductDTO digitalProductDTO) {
+        String response = digitalProductService.addDigitalProduct(digitalProductDTO);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping(path = "/edit/{id}")
+    public ResponseEntity<String> editDigitalProduct(@PathVariable long id, @RequestBody DigitalProductDTO digitalProductDTO) {
+        digitalProductDTO.setProductId(id);
+        String response = digitalProductService.editDigitalProduct(digitalProductDTO);
+        return ResponseEntity.ok(response);
+    }
+
+
+}
