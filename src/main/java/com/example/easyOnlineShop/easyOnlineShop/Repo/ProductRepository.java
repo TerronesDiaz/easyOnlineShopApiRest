054724cd11088c80b0f084db6e2197161d80aad8
TERRONES DIAZ FRANCISCO JAVIER <fterrones@ucol.mx>
2023-12-23 04:08:24 -0600

    Fix: Corrected the usage of PhysicalProduct instead of Product in the repository

--- src/main/java/com/example/easyOnlineShop/easyOnlineShop/Repo/ProductRepository.java
+++ src/main/java/com/example/easyOnlineShop/easyOnlineShop/Repo/ProductRepository.java
@@ -1,12 +1,12 @@
 package com.example.easyOnlineShop.easyOnlineShop.Repo;
 
-import com.example.easyOnlineShop.easyOnlineShop.Entity.PhysicalProduct;
+import com.example.easyOnlineShop.easyOnlineShop.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @EnableJpaRepositories
 @Repository
-public interface ProductRepository extends JpaRepository<PhysicalProduct, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
