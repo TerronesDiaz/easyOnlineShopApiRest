7da21905e67fbf82b3f8d406c4c02361c63ab87c
TERRONES DIAZ FRANCISCO JAVIER <fterrones@ucol.mx>
2023-12-30 01:35:51 -0600

    Update: Enhanced Security Configuration with JWT Authentication and Authorization
    - Modified securityFilterChain method to include JWT authentication and request authorization:
     + Specified /public/**, /v3/api-docs/**, /swagger-ui/**, /swagger-ui.html to be publicly accessible
     + Added .authenticationProvider(authProvider) to set the custom authentication provider.
     + Inserted jwtAuthenticationFilter before UsernamePasswordAuthenticationFilter to intercept requests for JWT processing.
    
     Note: The passwordEncoder bean method was removed in the updated configuration.

--- src/main/java/com/example/easyOnlineShop/easyOnlineShop/Config/SecurityConfig.java
+++ src/main/java/com/example/easyOnlineShop/easyOnlineShop/Config/SecurityConfig.java
@@ -1,26 +1,38 @@
 package com.example.easyOnlineShop.easyOnlineShop.Config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authProvider;
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder(); // Utiliza BCryptPasswordEncoder
-    }
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(sees -> sees.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authRequest -> authRequest
+                        .requestMatchers("/public/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .sessionManagement(sessionManager ->
+                        sessionManager
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-
 }
