b0e9d064222f6206f3b713ff8c32cea2dd7baa07
TERRONES DIAZ FRANCISCO JAVIER <fterrones@ucol.mx>
2023-12-30 01:57:23 -0600

    Add: Exception Handling Controller Advice
    - Created an AdviceController class to handle custom exceptions globally.
    - Implemented an exception handler method for CustomException.
    - The handler method constructs a custom error response with timestamp, status code, error message, and the exception message.
    - Responses are returned as ResponseEntity objects with appropriate HTTP status codes.

---
+++ src/main/java/com/example/easyOnlineShop/easyOnlineShop/AdviceController/AdviceController.java
@@ -0,0 +1,26 @@
+package com.example.easyOnlineShop.easyOnlineShop.AdviceController;
+
+import com.example.easyOnlineShop.easyOnlineShop.Exceptions.CustomException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class AdviceController {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Object> handleCustomException(CustomException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", ex.getStatus().value());
+        body.put("error", ex.getStatus().getReasonPhrase());
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, ex.getStatus());
+    }
+}
+
