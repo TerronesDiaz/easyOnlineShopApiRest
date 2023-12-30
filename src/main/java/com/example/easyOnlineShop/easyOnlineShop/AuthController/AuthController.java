41115f465b3dc33a288dc5050156d373bdd6c36e
TERRONES DIAZ FRANCISCO JAVIER <fterrones@ucol.mx>
2023-12-30 01:58:45 -0600

    Update: Updated AuthController for DTO Consistency
    - Renamed DTO classes in the AuthController for consistency with the service interface.
    - Changed AuthResponse to AuthResponseDTO, LoginRequest to LoginRequestDTO, and RegisterRequest to RegisterRequestDTO.

--- src/main/java/com/example/easyOnlineShop/easyOnlineShop/AuthController/AuthController.java
+++ src/main/java/com/example/easyOnlineShop/easyOnlineShop/AuthController/AuthController.java
@@ -1,8 +1,8 @@
 package com.example.easyOnlineShop.easyOnlineShop.AuthController;
 
-import com.example.easyOnlineShop.easyOnlineShop.Dto.AuthResponse;
-import com.example.easyOnlineShop.easyOnlineShop.Dto.LoginRequest;
-import com.example.easyOnlineShop.easyOnlineShop.Dto.RegisterRequest;
+import com.example.easyOnlineShop.easyOnlineShop.Dto.AuthResponseDTO;
+import com.example.easyOnlineShop.easyOnlineShop.Dto.LoginRequestDTO;
+import com.example.easyOnlineShop.easyOnlineShop.Dto.RegisterRequestDTO;
 import com.example.easyOnlineShop.easyOnlineShop.Service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +19,13 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping(value = "/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request)
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginRequestDTO request)
     {
         return ResponseEntity.ok(authService.login(request));
     }
 
     @PostMapping(value = "/register")
-    public ResponseEntity<AuthResponse>  register(@RequestBody RegisterRequest request)
+    public ResponseEntity<AuthResponseDTO>  register(@RequestBody RegisterRequestDTO request)
     {
         return ResponseEntity.ok(authService.register(request));
     }
